ed461c70d428ddf0ee0df78fa541784871281619
abhishekkvk007 <110897096+abhishekkvk007@users.noreply.github.com>
2022-08-09 02:52:37 -0700

    Change List

---
+++ change_list.py
@@ -0,0 +1,12 @@
+change_list=[1,2,3,4,5,"Hello","Job",True,False,None,[1,2,3,4,5,6,7,8,9,10]]
+print(change_list)
+change_list[0]="change1"
+print(change_list)
+change_list[-1]="change_Last"
+print(change_list)
+change_list[0:3]=["Hello"]
+print(change_list)
+change_list.insert(4,"Bobby")
+print(change_list)
+change_list.append("Appended")
+print(change_list)
